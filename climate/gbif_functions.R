ef0bc4810822b7422d29c94b2734913a1eef970c
tekritesh <ritesh.tekriwal03@gmail.com>
2022-07-05 00:51:24 +0800

    added functions; add R jupyter notebook

--- climate/gbif_functions.R
+++ climate/gbif_functions.R
@@ -6,6 +6,7 @@ require(geosphere)
 require(logger)
 
 
+
 # ============================= GBIF QUERY======================================
 fGetSpecies<-function(
   vSpeciesNames="",
@@ -321,5 +322,27 @@ if(F){
   
 }
 
-# =================================Stitching====================================
+# =================================Dist Haversine for DT========================
+
+dtHaversine <- 
+  function(
+    lat_from, 
+    lon_from, 
+    lat_to, 
+    lon_to,
+    r = 6378137){
+    
+    require(geosphere)
+    
+    radians <- pi/180
+    lat_to <- lat_to * radians
+    lat_from <- lat_from * radians
+    lon_to <- lon_to * radians
+    lon_from <- lon_from * radians
+    dLat <- (lat_to - lat_from)
+    dLon <- (lon_to - lon_from)
+    a <- (sin(dLat/2)^2) + (cos(lat_from) * cos(lat_to)) * (sin(dLon/2)^2)
+    
+  return(2 * atan2(sqrt(a), sqrt(1 - a)) * r)
+}
 
