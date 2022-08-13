99b6817bebade7408653f096e554086c4d6e9383
tekritesh <ritesh.tekriwal03@gmail.com>
2022-08-13 10:06:45 +0800

    WIP: repo clean up

--- streamlit_ui/pages/_About_Us.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.set_page_config(layout="wide",
-                   page_title="Land Cover",
-                   page_icon="🐸")
-
-f = open('assets/about.md', 'r')
-
-st.markdown(f.read())
